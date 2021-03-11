56acbf40de97575631b086bd4780b0e241c1113c
mallmann02 <leomallmann020302@gmail.com>
2021-03-11 19:30:36 -0300

    Requisito 2 - Permite usuário inserir uma imagem

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const memeTextoInput = document.querySelector('#text-input');
 const memeTexto = document.querySelector('#meme-text');
+const memeImagem = document.querySelector('#meme-image');
+const memeImagemInput = document.querySelector('#meme-insert');
 
 function escreverTextoNoMeme() {
   const textoDoInput = memeTextoInput.value;
   memeTexto.innerHTML = textoDoInput;
 }
 
+function readImage() {
+  if (this.files && this.files[0]) {
+    var file = new FileReader();
+    file.onload = function(e) {
+      document.getElementById("meme-image").src = e.target.result;
+    };       
+    file.readAsDataURL(this.files[0]);
+  }
+}
+
 memeTextoInput.addEventListener('change', escreverTextoNoMeme);
+memeImagemInput.addEventListener('change', readImage);
+
+
 
 // var fileTag = document.getElementById("filetag"),
 // preview = document.getElementById("preview");
