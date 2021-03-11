2563f3ee40815a74becbd7ec61a4e4da3cdde686
Lucas André <lucas.cancado89@gmail.com>
2021-03-11 16:37:06 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.getElementById('text-input').addEventListener('keyup', textGenerator);
 function loadFile(event) {
   document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
 }
+
+function fireBorder() {
+  document.getElementById('meme-image-container').style.border = 'dashed 3px red';
+}
+
+function waterBorder() {
+  document.getElementById('meme-image-container').style.border = 'double 5px blue';
+}
+
+function earthBorder() {
+  document.getElementById('meme-image-container').style.border = 'groove 6px green';
+}
+
+document.getElementById('fire').addEventListener('click', fireBorder);
+document.getElementById('water').addEventListener('click', waterBorder);
+document.getElementById('earth').addEventListener('click', earthBorder);
