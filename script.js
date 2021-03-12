ae1bbd81e7770c9b2a17ba10c2d4a8f3ebad7998
mallmann02 <leomallmann020302@gmail.com>
2021-03-11 23:44:14 -0300

    Requisito 6 + padrões de escrita do lint

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const memeTextoInput = document.querySelector('#text-input');
 const memeTexto = document.querySelector('#meme-text');
 const memeImagem = document.querySelector('#meme-image');
 const memeImagemInput = document.querySelector('#meme-insert');
+const mudarBordaButtons = document.querySelectorAll('.botao-mudar-borda');
 
 function escreverTextoNoMeme() {
   const textoDoInput = memeTextoInput.value;
@@ -9,39 +10,24 @@ function escreverTextoNoMeme() {
 }
 
 function readImage() {
+  const reader = new FileReader();
+
   if (this.files && this.files[0]) {
-    var file = new FileReader();
-    file.onload = function(e) {
-      document.getElementById("meme-image").src = e.target.result;
-    };       
-    file.readAsDataURL(this.files[0]);
+    reader.onload = (e) => {
+      memeImagem.src = e.target.result;
+    };
+    reader.readAsDataURL(this.files[0]);
   }
+}
+
+function mudarBorda(buttonEl) {
+  const botaoClicado = buttonEl.innerHTML;
+  const memeImagemContainer = document.querySelector('#meme-image-container');
+  if (botaoClicado === 'Fire') memeImagemContainer.style.border = '3px dashed red';
+  if (botaoClicado === 'Water') memeImagemContainer.style.border = '5px double blue';
+  if (botaoClicado === 'Earth') memeImagemContainer.style.border = '6px groove green';
 }
 
 memeTextoInput.addEventListener('change', escreverTextoNoMeme);
 memeImagemInput.addEventListener('change', readImage);
-
-
-
-// var fileTag = document.getElementById("filetag"),
-// preview = document.getElementById("preview");
-
-// fileTag.addEventListener("change", function() {
-// changeImage(this);
-// });
-
-// function changeImage(input) {
-  // var reader;
-
-  // if (input.files && input.files[0]) {
-    // reader = new FileReader();
-
-    // reader.onload = function(e) {
-    // preview.setAttribute('src', e.target.result);
-    // }
-
-    // reader.readAsDataURL(input.files[0]);
-//   }
-// }
-// <input type="file" id="filetag">
-// <img src="" id="preview"> 
+mudarBordaButtons.forEach((el) => el.addEventListener('click', (e) => mudarBorda(e.target)));
