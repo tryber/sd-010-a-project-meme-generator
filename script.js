8cceefcf194cb574c4260894f25fd59f31735091
mallmann02 <leomallmann020302@gmail.com>
2021-03-12 00:37:47 -0300

    Requisito 7 - Usuário pode usar imagens prontas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const memeTexto = document.querySelector('#meme-text');
 const memeImagem = document.querySelector('#meme-image');
 const memeImagemInput = document.querySelector('#meme-insert');
 const mudarBordaButtons = document.querySelectorAll('.botao-mudar-borda');
+const imagensSugeridas = document.querySelectorAll('#imagens-sugestao img');
 
 function escreverTextoNoMeme() {
   const textoDoInput = memeTextoInput.value;
@@ -28,6 +29,12 @@ function mudarBorda(buttonEl) {
   if (botaoClicado === 'Earth') memeImagemContainer.style.border = '6px groove green';
 }
 
+function usarImagemSugerida(imgElHTML) {
+  const imagemSelecionada = imgElHTML.src;
+  memeImagem.src = imagemSelecionada;
+}
+
 memeTextoInput.addEventListener('change', escreverTextoNoMeme);
 memeImagemInput.addEventListener('change', readImage);
 mudarBordaButtons.forEach((el) => el.addEventListener('click', (e) => mudarBorda(e.target)));
+imagensSugeridas.forEach((el) => el.addEventListener('click', (e) => usarImagemSugerida(e.target)));
