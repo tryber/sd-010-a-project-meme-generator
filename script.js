5dbfaccdbba698ad5d37a2bd8194a55d3b7005e7
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-11 21:54:52 -0300

    terceiro requisito

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const textBox = document.getElementById('text-input');
-textBox.addEventListener('keydown', () => {
-  const getParagraph = document.getElementById('meme-text');
-  getParagraph.innerText = textBox.value;
+function putText() {
+  const textBox = document.getElementById('text-input');
+
+  textBox.addEventListener('keyup', () => {
+    const getParagraph = document.getElementById('meme-text');
+    getParagraph.innerText = textBox.value;
 });
+}
+
+let getBtn = document.getElementById('meme-insert')
+getBtn.addEventListener('click', () => {
+  let image = document.getElementById('meme-insert').files[0].name
+  document.getElementById('meme-image').src = image
+})
+
+
+// let getBtn = document.getElementById('meme-image')
+// let image = document.getElementById('meme-insert').files[0].name
+// getBtn.src = image
+
+putText()
+
