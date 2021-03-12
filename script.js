e8aca32fc6927fa6f0c029b900172a5b3e4f02e4
Lucas Pedroso <lepsk8@gmail.com>
2021-03-12 01:02:05 -0300

    Finish Project Memes Generator.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const inputText = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeImg = document.querySelector('#meme-image');
+const memeInsert = document.querySelector('#meme-insert');
+const errorMsg = document.querySelector('#error-msg');
+const memeImageContainer = document.querySelector('#meme-image-container');
 
 const addEvListener = (elementNode, event, fn) => {
   if (typeof fn !== 'function') {
@@ -14,55 +18,38 @@ const showInputTextInContainer = () => {
   memeText.textContent = inputText.value;
 };
 
-// ==========================================================================
-const createTodoItem = (text) => {
-  const li = document.createElement('li');
-  li.innerText = text;
-  li.addEventListener('dblclick', (e) => { changeLineThrough(e, 'completed'); });
-  li.addEventListener('click', (e) => { changeColor(e, colorGray); });
-  return li;
-};
-
-const moveTodoItem = (count) => {
-  let selected;
-  let insertLocal;
-  let indexItem;
-  let listLength;
-  const listTodoItem = getAllTodoList();
-  for (let index = 0; index < listTodoItem.length; index += 1) {
-    const element = listTodoItem[index];
-    if (element.style.backgroundColor === colorGray) {
-      selected = listTodoItem[index];
-      indexItem = index;
-      listLength = listTodoItem.length;
-      insertLocal = listTodoItem[index + count];
-    }
-  }
-  return { selected, indexItem, listLength, insertLocal };
-};
-
-const moveDown = () => {
-  const { selected, indexItem, listLength, insertLocal } = moveTodoItem(2);
-  if (!selected) return;
-  if (indexItem === listLength) {
+const fileReader = (e) => {
+  const file = new FileReader();
+  if (!e.target.files[0]) {
     return;
   }
 
-  const listContainer = getListContainer();
-  listContainer.insertBefore(selected, insertLocal);
+  file.onloadstart = () => { memeImg.setAttribute('src', ''); errorMsg.innerHTML = ''; };
+
+  file.onprogress = () => { memeText.textContent = 'Carregando...'; };
+
+  file.onerror = () => { errorMsg.innerHTML = 'Ocorreu um erro inesperado, tente inserir uma imagem novamente.'; };
+
+  file.onloadend = (ev) => {
+    memeText.textContent = inputText.value;
+    memeImg.setAttribute('src', ev.target.result);
+  };
+
+  file.readAsDataURL(e.target.files[0]);
 };
 
-const moveUp = () => {
-  const { selected, indexItem, insertLocal } = moveTodoItem(-1);
-  if (!selected) return;
-  if (indexItem === 0) {
-    return;
-  }
-
-  const listContainer = getListContainer();
-  listContainer.insertBefore(selected, insertLocal);
+const getImage = (imgId) => {
+  return document.querySelector(imgId).src;
 };
 
 window.onload = () => {
   addEvListener('#text-input', 'keyup', showInputTextInContainer);
+  addEvListener('#meme-insert', 'change', fileReader);
+  addEvListener('#fire', 'click', () => { memeImageContainer.className = 'border-fire'; });
+  addEvListener('#earth', 'click', () => { memeImageContainer.className = 'border-earth'; });
+  addEvListener('#water', 'click', () => { memeImageContainer.className = 'border-water'; });
+  addEvListener('#meme-1', 'click', () => { memeImg.setAttribute('src', getImage('#meme-1')); });
+  addEvListener('#meme-2', 'click', () => { memeImg.setAttribute('src', getImage('#meme-2')); });
+  addEvListener('#meme-3', 'click', () => { memeImg.setAttribute('src', getImage('#meme-3')); });
+  addEvListener('#meme-4', 'click', () => { memeImg.setAttribute('src', getImage('#meme-4')); });
 };
