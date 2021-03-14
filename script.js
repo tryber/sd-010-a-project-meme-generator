92f3159a2e28470d6de53d2a903d55e3cb66f1d0
Gabriel Ferreira <glucas709@gmail.com>
2021-03-14 18:48:48 -0300

    Requisito 02 - Inserindo imagem e texto

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const imageUpload = document.getElementById('meme-insert').value;
+const containerMeme = document.getElementById('meme-image-container');
+let imageOut = document.getElementById('meme-image');
 
 inputText.addEventListener('keyup', () => {
   memeText.innerHTML = inputText.value;
 });
+
+let loadImage = function (event) {
+  imageOut.src = URL.createObjectURL(event.target.files[0]);
+  imageOut.onload = function () {
+    URL.revokeObjectURL(imageOut.src);
+  }
+  imageOut.style.objectFit = 'cover';
+}
+
+console.log(containerMeme);
