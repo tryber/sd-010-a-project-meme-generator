7d6bde9cb1819b9f4cd301d84e4a8203ad6300c9
Gabriel Ferreira <glucas709@gmail.com>
2021-03-14 19:50:35 -0300

    Requisito 05 - Limitando tamanho do texto

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const maxChars = document.getElementById('max-chars');
+const memeInsert = document.getElementById('meme-insert');
+
+maxChars.innerHTML = 'Max. Chars: 0/60';
 
 inputText.addEventListener('keyup', () => {
   memeText.innerHTML = inputText.value;
+  const count = inputText.value.replace(/\s/g, '');
+  maxChars.innerHTML = `Max. Chars: ${count.length}/60`;
 });
 
-const loadImage = function (event) {
+memeInsert.onchange = (event) => {
   const imageOut = document.getElementById('meme-image');
   imageOut.src = URL.createObjectURL(event.target.files[0]);
-  imageOut.onload = function () {
+  imageOut.onload = () => {
     URL.revokeObjectURL(imageOut.src);
   };
 };
