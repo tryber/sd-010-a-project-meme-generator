e0161774116a3eb21845a335aec2c2de64909bdc
Gabriel Ferreira <glucas709@gmail.com>
2021-03-14 20:33:46 -0300

    Requisito 06 - Mudando estilo da borda

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const maxChars = document.getElementById('max-chars');
 const memeInsert = document.getElementById('meme-insert');
+const memeContainer = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 maxChars.innerHTML = 'Max. Chars: 0/60';
 
@@ -18,3 +22,13 @@ memeInsert.onchange = (event) => {
     URL.revokeObjectURL(imageOut.src);
   };
 };
+
+fire.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed red';
+});
+water.addEventListener('click', () => {
+  memeContainer.style.border = '5px double blue';
+});
+earth.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove green';
+});
