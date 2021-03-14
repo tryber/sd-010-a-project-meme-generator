edab7c96c8d20783bd51b1cff94be94ee650a5e1
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-14 11:21:31 -0300

    lint

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ function putText() {
   textBox.addEventListener('keyup', () => {
     const getParagraph = document.getElementById('meme-text');
     getParagraph.innerText = textBox.value;
-});
+  });
 }
 
-let getBtn = document.getElementById('meme-insert')
+const getBtn = document.getElementById('meme-insert');
 getBtn.addEventListener('click', () => {
-  let image = document.getElementById('meme-insert').files[0].name
-  document.getElementById('meme-image').src = image
-})
-
+  const image = document.getElementById('meme-insert').files[0].name;
+  document.getElementById('meme-image').src = image;
+});
 
 // let getBtn = document.getElementById('meme-image')
 // let image = document.getElementById('meme-insert').files[0].name
 // getBtn.src = image
 
-putText()
-
+putText();
