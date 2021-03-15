c736ec221a57a5a6a4370d95c0dcb8a2b5615337
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-15 12:11:55 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ inputText.addEventListener('keyup', () => {
   const memeText = document.querySelector('#meme-text');
   memeText.innerText = inputText.value;
 });
+
+document.querySelector('#meme-insert').addEventListener('click', () => {
+  const image = document.querySelector('#meme-insert').files[0].name;
+  document.querySelector('#meme-image').src = image;
+});
