88517ad04db3c01e7ef1042ffb2a5de137d3ef1a
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-14 21:04:28 -0300

    Requisito 01 - input de texto

---
+++ script.js
@@ -0,0 +1,9 @@
+const textInput = document.getElementById('text-input');
+const textSpan = document.getElementById('meme-text');
+
+function inputText(evt) {
+  evt.preventDefault();
+  textSpan.innerText = textInput.value;
+}
+
+textInput.addEventListener('change', inputText);
