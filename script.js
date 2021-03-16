2133d49ea4ae2ea5a74e4252f49824e4e1b39a7f
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-16 19:53:09 -0300

    Requisito 2 - texto sobre imagem

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const memeText = document.getElementById('meme-text');
 inputTextField.addEventListener('keyup', () => {
   let inputText = inputTextField.value;
   memeText.innerText = inputText;
-})
+});
 
+function loadImage(event) {
+  let memeImage = document.getElementById('meme-image');
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src) //free memory
+  }
+}
