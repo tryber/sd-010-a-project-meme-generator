dc750b5b001b290cfc3e8ab688e09dd2bfebdaa1
rogerio <rogerio.cl@gmail.com>
2021-03-17 18:52:17 -0300

    primeiro requisito

---
+++ script.js
@@ -0,0 +1,12 @@
+// Declaração de variáveis globais
+const textInsert = document.getElementById('text-input');
+const textMeme = document.getElementById('meme-text');
+const image = document.getElementById('meme-image');
+const insertImage = document.getElementById('meme-insert');
+const 
+
+// Declaração de funções
+
+// Delaração de escutadores de eventos
+
+// Ações de inicialização 
