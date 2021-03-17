5dd6cdd9339a87ebf3068e7042acecfe10f0c802
rogerio <rogerio.cl@gmail.com>
2021-03-17 19:38:42 -0300

    primeiro requisito corrigido

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const textInsert = document.getElementById('text-input');
 const textMeme = document.getElementById('meme-text');
 const image = document.getElementById('meme-image');
 const insertImage = document.getElementById('meme-insert');
-const 
+
 
 // Declaração de funções
+function inserTextMeme() {
+  textMeme.innerText = textInsert.value;
+}
+
 
 // Delaração de escutadores de eventos
+textInsert.addEventListener('input', inserTextMeme);
 
 // Ações de inicialização 
