5d170ab61e8590e35bbbaf1a018530b75685934f
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-20 12:57:39 -0300

    Requirement 1 Done

---
+++ script.js
@@ -0,0 +1,3 @@
+const selectInputText = document.querySelector('#text-input');
+const selectDiv = document.querySelector('#meme-image-container');
+const selectText = document.querySelector('#meme-text');
