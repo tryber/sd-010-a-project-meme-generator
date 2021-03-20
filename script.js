35d73e1b450690f5ccc2fb8d948e2ff83b6c3bb5
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-20 20:53:30 -0300

    Requirement 3 Done

--- script.js
+++ script.js
@@ -4,40 +4,7 @@ const selectText = document.querySelector('#meme-text');
 const selectInputFile = document.querySelector('#meme-insert');
 const selectMemeImage = document.querySelector('#meme-image');
 
-// const loadImage = (file) => {
-//   const reader = new FileReader();
-
-//   reader.onloadend = (e) => {
-//     const image = selectMemeImage;
-//     image.src = e.target.result;
-//   };
-
-//   reader.readAsDataURL(file);
-// };
-
-// function loadImage(event) {
-//   selectMemeImage.src = URL.createObjectURL(event.target.files[0]);
-//   selectMemeImage.onload = function() {
-//     selectMemeImage.onload = function() {
-//       URL.revokeObjectURL(selectMemeImage.src);// free memory
-//     };
-//   };
-//   selectMemeImage.src = selectInputFile.value;
-// }
-
-// {/* <input type="file" accept="image/*" onchange="loadFile(event)">
-// <img id="output"/>
-// <script>
-//   var loadFile = function(event) {
-//     var output = document.getElementById('output');
-//     output.src = URL.createObjectURL(event.target.files[0]);
-//     output.onload = function() {
-//       URL.revokeObjectURL(output.src) // free memory
-//     }
-//   };
-// </script> */}
-
-function loadImage() {
+function loadImage() { // consultado o uso desta função em https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
   selectInputFile.addEventListener('change', (event) => {
     selectMemeImage.style.width = '100%';
     selectMemeImage.style.height = '100%';
