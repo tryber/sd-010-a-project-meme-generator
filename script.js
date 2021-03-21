b1cd9e1d81efed213f3b4db61597d3ff1dfe0d44
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-20 22:16:19 -0300

    Requirement 7 Done

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const selectMemeImage = document.querySelector('#meme-image');
 const selectButtonFire = document.querySelector('#fire');
 const selectButtonWater = document.querySelector('#water');
 const selectButtonEarth = document.querySelector('#earth');
+const selectMeme1 = document.querySelector('#meme-1');
+const selectMeme2 = document.querySelector('#meme-2');
+const selectMeme3 = document.querySelector('#meme-3');
+const selectMeme4 = document.querySelector('#meme-4');
 
 function loadImage() { // consultado o uso desta função em https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
   selectInputFile.addEventListener('change', (event) => {
@@ -39,3 +43,26 @@ selectInputText.addEventListener('keyup', addTextMeme);
 selectButtonFire.addEventListener('click', borderFire);
 selectButtonWater.addEventListener('click', borderWater);
 selectButtonEarth.addEventListener('click', borderEarth);
+selectMeme1.addEventListener('click', () => {
+  selectMemeImage.style.width = '100%';
+  selectMemeImage.style.height = '100%';
+  selectMemeImage.src = 'imgs/meme1.png';
+});
+
+selectMeme2.addEventListener('click', () => {
+  selectMemeImage.style.width = '100%';
+  selectMemeImage.style.height = '100%';
+  selectMemeImage.src = 'imgs/meme2.png';
+});
+
+selectMeme3.addEventListener('click', () => {
+  selectMemeImage.style.width = '100%';
+  selectMemeImage.style.height = '100%';
+  selectMemeImage.src = 'imgs/meme3.png';
+});
+
+selectMeme4.addEventListener('click', () => {
+  selectMemeImage.style.width = '100%';
+  selectMemeImage.style.height = '100%';
+  selectMemeImage.src = 'imgs/meme4.png';
+});
