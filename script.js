96fd7719ede81f6b8f6fc7a3708880ccc18244e9
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-04-27 19:11:25 -0300

    sexto requisito

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 function putText() {
   const textBox = document.getElementById('text-input');
-
   textBox.addEventListener('keyup', () => {
     const getParagraph = document.getElementById('meme-text');
     getParagraph.innerText = textBox.value;
   });
 }
 
-const getBtn = document.getElementById('meme-insert');
-getBtn.addEventListener('click', () => {
-  const image = document.getElementById('meme-insert').files[0].name;
-  document.getElementById('meme-image').src = image;
-});
+function putImage() {
+  const getBtn = document.getElementById('meme-insert');
+  getBtn.addEventListener('click', () => {
+    const image = document.getElementById('meme-insert').files[0].name;
+    document.getElementById('meme-image').src = image;
+  });
+}
 
-// let getBtn = document.getElementById('meme-image')
-// let image = document.getElementById('meme-insert').files[0].name
-// getBtn.src = image
+function frameListeners() {
+  const getBorder = document.getElementById('meme-image-container')
+  document.getElementById('fire').addEventListener('click', function() {
+    getBorder.style.border = '3px dashed red'
+  })
+  document.getElementById('water').addEventListener('click', function() {
+    getBorder.style.border = '5px double blue'
+  })
+  document.getElementById('earth').addEventListener('click', function() {
+    getBorder.style.border = '6px groove green'
+  })
+}
 
-putText();
+
+
+window.onload = function() {
+  putText();
+  frameListeners()
+  putImage() 
+}
